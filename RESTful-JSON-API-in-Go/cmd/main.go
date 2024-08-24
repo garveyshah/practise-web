74c3cdc0639774af511012be0d896f7e37ba076d
oumouma <garveyshah1595@gmail.com>
2024-08-24 23:10:53 +0300

    feat:templates/, cmd/ -  Code to parse hmtl and serve it to the net

---
+++ RESTful-JSON-API-in-Go/cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("../templates/home.html"))
+		err := tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Failed to render template", http.StatusInternalServerError)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
