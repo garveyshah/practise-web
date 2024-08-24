b437dd8cf227ca4a6d889e2ce5f07df8d6de2806
oumouma <garveyshah1595@gmail.com>
2024-08-24 23:45:48 +0300

    update: error handling

--- RESTful-JSON-API-in-Go/cmd/main.go
+++ RESTful-JSON-API-in-Go/cmd/main.go
@@ -11,9 +11,11 @@ func main() {
 		tmpl := template.Must(template.ParseFiles("../templates/home.html"))
 		err := tmpl.Execute(w, nil)
 		if err != nil {
+			log.Printf("Template execution failed: %v", err)
 			http.Error(w, "Failed to render template", http.StatusInternalServerError)
 		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+ 
